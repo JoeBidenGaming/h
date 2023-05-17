ad939a4772dbb8e5808c9c45648aac2f189e4c03
JoeBidenGaming <22jk1318@barton.ac.uk>
2023-05-17 13:19:29 +0100

    random letter test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Read();
-            Console.WriteLine("Hello world");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Random random = new Random();
+            string letter = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                if (random.Next(1, 4) == 1)
+                {
+                    letter = "a";
+                }
+                else if (random.Next(1, 4)== 2)
+                {
+                    letter="b";
+                }
+                else if(random.Next(1,4) == 3)
+                {
+                    letter = "c";
+                }
+                else if ((random.Next(1, 4) > 4))
+                {
+                    letter = "high";
+                }
+                else if (((random.Next(1, 4))< 1))
+                {
+                    letter = "low";
+                }
+                Console.Write(letter);
+            }
+            Console.ReadKey();
         }
     }
 }
